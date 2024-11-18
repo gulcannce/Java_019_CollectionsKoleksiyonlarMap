71069ca058df71f28263de68c82d3974ce4f9709
Gülcan çelik <gulcanncee@gmail.com>
2024-11-18 20:29:09 +0300

    Update AppMain.java

--- src/main/java/com/gulcancelik/AppMain.java
+++ src/main/java/com/gulcancelik/AppMain.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.TreeMap; 
 
 
 public class AppMain {
